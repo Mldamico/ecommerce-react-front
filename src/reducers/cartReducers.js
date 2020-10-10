16603af3c9523d75641246de8a79170ba8d479a6
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2020-10-10 10:21:05 -0300

    Cart actions and reducer

---
+++ src/reducers/cartReducers.js
@@ -0,0 +1,30 @@
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants';
+
+export const cartReducers = (state = { cartItems: [] }, action) => {
+  switch (action.type) {
+    case CART_ADD_ITEM:
+      const item = action.payload;
+      const existItem = state.cartItems.find(
+        (itemInCart) => itemInCart.product === item.product
+      );
+
+      if (existItem) {
+        return {
+          ...state,
+          cartItems: state.cartItems.map((itemInCart) =>
+            itemInCart.product === existItem.product ? item : itemInCart
+          ),
+        };
+      } else {
+        return {
+          ...state,
+          cartItems: [...state.cartItems, item],
+        };
+      }
+      return {};
+    case CART_REMOVE_ITEM:
+      return {};
+    default:
+      return state;
+  }
+};
