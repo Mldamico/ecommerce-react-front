d2b73d1ddf9adfb61cd255e5180ef692a7aee906
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2020-10-13 11:29:50 -0300

    Order Screen

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import { ProfileScreen } from './screens/ProfileScreen';
 import { ShippingScreen } from './screens/ShippingScreen';
 import { PaymentScreen } from './screens/PaymentScreen';
 import { PlaceOrderScreen } from './screens/PlaceOrderScreen';
+import { OrderScreen } from './screens/OrderScreen';
 function App() {
   return (
     <Router>
@@ -23,6 +24,7 @@ function App() {
           <Route path='/profile' component={ProfileScreen} />
           <Route path='/shipping' component={ShippingScreen} />
           <Route path='/payment' component={PaymentScreen} />
+          <Route path='/order/:id' component={OrderScreen} />
           <Route path='/placeorder' component={PlaceOrderScreen} />
           <Route path='/login' component={LoginScreen} />
           <Route path='/register' component={RegisterScreen} />
